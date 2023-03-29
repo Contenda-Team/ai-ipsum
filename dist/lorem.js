2949490762d36850fe2d4734ae600f00c29e6e2c
Cassidy Williams <cassidoo@users.noreply.github.com>
2023-03-29 14:11:55 -0500

    Add basic HTML page showing usage of AI Ipsum

---
+++ dist/lorem.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+	const quantity = document.querySelector("#quantity");
+	fetch(`/ipsum/${quantity.value}`)
+		.then((response) => response.json())
+		.then((data) => {
+			const display = document.querySelector("#sample");
+			display.innerHTML = `<h3>${
+				data.words
+			} words of AI Ipsum</h3><p>${data.ipsum.replace(
+				/(?:\r\n|\r|\n)/g,
+				"</p><p>"
+			)}</p>`;
+		});
+});
